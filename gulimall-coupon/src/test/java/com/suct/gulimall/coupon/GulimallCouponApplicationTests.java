61c8bd7baf79e415a35237fa0778593d502a2807
lyh <13063527966@163.com>
2023-03-07 10:42:11 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/suct/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suct.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-order/src/test/java/com/scut/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scut.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
